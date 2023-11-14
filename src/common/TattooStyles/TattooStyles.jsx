425f6f6e68e94afadb52c74f457129a59b647076
Borja <pintocorreas@gmail.com>
2023-11-14 21:23:49 +0100

    feat: cleaning code

--- src/common/TattooStyles/TattooStyles.jsx
+++ src/common/TattooStyles/TattooStyles.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import './TattooStyles.css';
+import React, { useState } from "react";
+import "./TattooStyles.css";
 
 export const TattooStyles = () => {
   const [isFlipped1, setIsFlipped1] = useState(false);
@@ -20,16 +20,47 @@ export const TattooStyles = () => {
 
   return (
     <div className="tattooStyles">
-      <div className="background-banner stylesBanner" style={{ backgroundImage: `url('../../img/home-example.jpg')` }}>
+      <div
+        className="background-banner stylesBanner"
+        style={{ backgroundImage: `url('../../img/home-example.jpg')` }}
+      >
         <div className="image-container">
-          <div className={`image-wrapper ${isFlipped1 ? 'flipped' : ''}`} onClick={handleFlip1}>
-            <img className='image-tattoo' src={isFlipped1 ? './src/img/tattoo1.jpg' : './src/img/tattoo5.png'} alt={`Tattoo ${isFlipped1 ? '1B' : '1A'}`} />
+          <div
+            id="rockRoll"
+            className={`image-wrapper ${isFlipped1 ? "flipped" : ""}`}
+            onClick={handleFlip1}
+          >
+            <img
+              className="image-tattoo"
+              src={
+                isFlipped1 ? "./src/img/tattoo1.jpg" : "./src/img/tattoo5.png"
+              }
+              alt={`Tattoo ${isFlipped1 ? "1B" : "1A"}`}
+            />
           </div>
-          <div className={`image-wrapper ${isFlipped2 ? 'flipped' : ''}`} onClick={handleFlip2}>
-            <img className='image-tattoo' src={isFlipped2 ? './src/img/tattoo2.jpg' : './src/img/tattoo4.jpg'} alt={`Tattoo ${isFlipped2 ? '2B' : '2A'}`} />
+          <div
+            className={`image-wrapper ${isFlipped2 ? "flipped" : ""}`}
+            onClick={handleFlip2}
+          >
+            <img
+              className="image-tattoo"
+              src={
+                isFlipped2 ? "./src/img/tattoo2.jpg" : "./src/img/tattoo4.jpg"
+              }
+              alt={`Tattoo ${isFlipped2 ? "2B" : "2A"}`}
+            />
           </div>
-          <div className={`image-wrapper ${isFlipped3 ? 'flipped' : ''}`} onClick={handleFlip3}>
-            <img className='image-tattoo' src={isFlipped3 ? './src/img/tattoo3.jpg' : './src/img/tattoo6.jpg'} alt={`Tattoo ${isFlipped3 ? '3B' : '3A'}`} />
+          <div
+            className={`image-wrapper ${isFlipped3 ? "flipped" : ""}`}
+            onClick={handleFlip3}
+          >
+            <img
+              className="image-tattoo"
+              src={
+                isFlipped3 ? "./src/img/tattoo3.jpg" : "./src/img/tattoo6.jpg"
+              }
+              alt={`Tattoo ${isFlipped3 ? "3B" : "3A"}`}
+            />
           </div>
         </div>
       </div>
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -3,20 +3,15 @@ import axios from 'axios';
 export const logUser = async (body) => {
 
     console.log(body);
-
-    // SIMULACRO DE CONEXION REAL A API
     let search =  await axios.post(`http://localhost:4004/user/login`, body);
     return search.data.token
 }
 
 export const registerUser = async (body) => {
-
     console.log(body);
-
     return await axios.post(`http://localhost:4004/user/register`, body);
 }
 
 export const bringTattooArtists = async () => {
-
     return await axios.get(`https://localhost:4004/artist/all`);
 }
